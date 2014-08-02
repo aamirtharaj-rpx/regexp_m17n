8c3ce92a0f50db04ce25c6fa90414c84184ac4be
Antony Raj <antony.java@gmail.com>
2014-08-02 23:45:45 +0530

    Fixing encoding issue for dummy encoding

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,7 @@
+require 'pry'
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    return true if str != nil && str != '' && str.encoding.dummy?
+    str =~ Regexp.new('^.+$'.encode(str.encoding))
   end
 end
